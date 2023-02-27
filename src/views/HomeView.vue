561393ce607eed9623ee14d360458e455884227e
Flávio Mota <fmota@student.42lisboa.com>
2023-02-27 10:06:33 +0000

    searching setting from api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,7 +16,20 @@
 			<input type="submit" value="Search" />
 		</form>
 
-		<div class="movies-list">MOVIES</div>
+		<div class="movies-list">
+			<div class="movie" v-for="movie in movies" :key="movie.imdbID">
+				<router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+					<div class="product-image">
+						<img :src="movie.Poster" alt="Movie Poster" />
+						<div class="type">{{ movie.Type }}</div>
+					</div>
+					<div class="detail">
+						<p class="year">{{ movie.Year }}</p>
+						<h3>{{ movie.Title }}</h3>
+					</div>
+				</router-link>
+			</div>
+		</div>
 
   </div>
 </template>
@@ -36,8 +49,10 @@ export default {
 				fetch(`https://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
 					.then(response => response.json())
 					.then(data => {
-						console.log(data);
-					})
+						movies.value = data.Search;
+						search.value = "";
+
+					});
 			}
 		}
 
@@ -131,6 +146,64 @@ export default {
 
 				&:active {
 					background-color: #3B8070;
+				}
+			}
+		}
+	}
+
+	.movies-list {
+		display: flex;
+		flex-wrap: wrap;
+		margin: 0px 8px;
+
+		.movie {
+			max-width: 50%;
+			flex: 1 1 50%;
+			padding: 16px 8px;
+
+			.movie-link {
+				display: flex;
+				flex-direction: column;
+				height: 100%;
+
+				.product-image {
+					position: relative;
+					display: block;
+
+					img {
+						display: block;
+						width: 100%;
+						height: 275px;
+						object-fit: cover;
+					}
+
+					.type {
+						position: absolute;
+						padding: 8px 16px;
+						background-color: #42B883;
+						color: #fff;
+						bottom: 16px;
+						left: 0px;
+						text-transform: capitalize;
+					}
+				}
+
+				.detail {
+					background-color: #496583;
+					padding: 16px 8px;
+					flex: 1 1 100%;
+					border-radius: 0px 0px 8px 8px;
+
+					.year {
+						color: #AAA;
+						font-size: 14px;
+					}
+
+					h3 {
+						color: #FFF;
+						font-weight: 600;
+						font-size: 18px;
+					}
 				}
 			}
 		}
